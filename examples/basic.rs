6c0461c3a4f2bec197a7b18a57602719614b96d2
Yuki Kodama <endflow.net@gmail.com>
2019-09-25 23:08:15 +0900

    Support filter feature

--- examples/basic.rs
+++ examples/basic.rs
@@ -6,42 +6,73 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Data {
     pub name: String,
-    pub message: String,
+    pub age: i32,
+    pub login: bool,
 }
 
 impl Data {
-    fn new(name: &str, message: &str) -> Data {
+    fn new(name: &str, age: i32, login: bool) -> Data {
         Data {
             name: name.to_string(),
-            message: message.to_string(),
+            age,
+            login,
         }
     }
 }
 
 fn main() -> Result<(), Error> {
-    let client = Client::new("box_ed82aef3f93176996145");
+    let client = Client::new("box_8ed82aef3f93176996145");
 
-    let mut data = Data::new("kuy", "Hello, Jsonbox!");
-    let (record, meta) = client.create(&data)?;
-    println!("CREATE: data={:?}, meta={:?}", record, meta);
+    let data = Data::new("jsonbox", 42, false);
+    let (record, meta1) = client.create(&data)?;
+    println!("CREATE: data={:?}, meta={:?}", record, meta1);
 
-    let (record, meta) = client.read().id::<Data>(&meta.id)?;
-    println!("READ: data={:?}, meta={:?}", record, meta);
+    let (record, meta1) = client.read().id::<Data>(&meta1.id)?;
+    println!("READ single: data={:?}, meta={:?}", record, meta1);
+
+    let mut data = Data::new("kuy", 16, false);
+    let (record, meta2) = client.create(&data)?;
+    println!("CREATE: data={:?}, meta={:?}", record, meta2);
 
     let all = client.read().all::<Data>()?;
     println!("READ: len={}, all={:?}", all.len(), all);
 
-    let asc = client.read().order_by("createdOn").run::<Data>()?;
+    let asc = client.read().order_by("age").run::<Data>()?;
     println!("READ: len={}, asc={:?}", asc.len(), asc);
 
-    let few = client.read().limit(3).run::<Data>()?;
+    let few = client.read().limit(1).run::<Data>()?;
     println!("READ: len={}, few={:?}", few.len(), few);
 
-    data.message = format!("Hello, GitHub! [{}]", meta.id);
-    let _ = client.update(&meta.id, &data)?;
+    data.name = format!("kuy {}", meta2.id);
+    client.update(&meta2.id, &data)?;
     println!("UPDATE: OK");
 
-    let _ = client.delete(&meta.id)?;
+    let filtered = client
+        .read()
+        .filter_by("name:{}", &data.name)
+        .run::<Data>()?;
+    println!("READ: len={}, filtered={:?}", filtered.len(), filtered);
+
+    let filtered = client.read().filter_by("name:{}*", "kuy").run::<Data>()?;
+    println!("READ: len={}, filtered={:?}", filtered.len(), filtered);
+
+    let filtered = client
+        .read()
+        .filter_by("name:*{}", &meta2.id)
+        .run::<Data>()?;
+    println!("READ: len={}, filtered={:?}", filtered.len(), filtered);
+
+    data.age = 8;
+    client.update(&meta2.id, &data)?;
+    println!("UPDATE: OK");
+
+    let filtered = client.read().filter_by("age:<{}", 10).run::<Data>()?;
+    println!("READ: len={}, filtered={:?}", filtered.len(), filtered);
+
+    client.delete(&meta1.id)?;
+    println!("DELETE: OK");
+
+    client.delete(&meta2.id)?;
     println!("DELETE: OK");
 
     Ok(())
--- src/query_builder.rs
+++ src/query_builder.rs
@@ -1,4 +1,6 @@
+use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
 use serde::de::DeserializeOwned;
+use std::fmt::Display;
 
 use crate::client::{Client, Meta};
 use crate::error::Result;
@@ -14,7 +16,7 @@ pub struct QueryBuilder<'a> {
     sort: Order,
     skip: u32,
     limit: u32,
-    q: Option<String>,
+    q: Vec<String>,
 }
 
 impl<'a> QueryBuilder<'a> {
@@ -24,7 +26,7 @@ impl<'a> QueryBuilder<'a> {
             sort: Order::Desc("_createdOn".into()),
             skip: 0,
             limit: 20,
-            q: None,
+            q: vec![],
         }
     }
 
@@ -72,6 +74,19 @@ impl<'a> QueryBuilder<'a> {
         }
     }
 
+    pub fn filter_by<T: Display>(&self, format: &str, value: T) -> QueryBuilder {
+        let value = utf8_percent_encode(&format!("{}", value), NON_ALPHANUMERIC).to_string();
+        let mut q = self.q.clone();
+        q.push(format.replace("{}", &value));
+        QueryBuilder {
+            client: self.client,
+            sort: self.sort.clone(),
+            skip: self.skip,
+            limit: self.limit,
+            q,
+        }
+    }
+
     pub fn id<T>(&self, id: &str) -> Result<(T, Meta)>
     where
         T: DeserializeOwned,
@@ -95,12 +110,16 @@ impl<'a> QueryBuilder<'a> {
     }
 
     pub fn to_string(&self) -> String {
-        format!(
+        let mut query = format!(
             "sort={}&skip={}&limit={}",
             self.sort_string(),
             self.skip,
             self.limit
-        )
+        );
+        if self.q.len() > 0 {
+            query = format!("{}&q={}", query, self.filter_string());
+        }
+        query
     }
 
     fn sort_string(&self) -> String {
@@ -108,5 +127,54 @@ impl<'a> QueryBuilder<'a> {
             Order::Asc(field) => field.to_string(),
             Order::Desc(field) => format!("-{}", field),
         }
+    }
+
+    fn filter_string(&self) -> String {
+        let mut filter = self
+            .q
+            .iter()
+            .fold(String::new(), |acc, q| format!("{}{},", acc, q));
+        filter.pop();
+        filter
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sort_string() {
+        let c = Client::new("xxx");
+        let q = QueryBuilder::new(&c);
+        assert_eq!(q.sort_string(), "-_createdOn");
+
+        let q = q.order_by("name");
+        assert_eq!(q.sort_string(), "name");
+
+        let q = q.desc();
+        assert_eq!(q.sort_string(), "-name");
+    }
+
+    #[test]
+    fn test_filter_string() {
+        let c = Client::new("xxx");
+        let q = QueryBuilder::new(&c);
+        assert_eq!(q.filter_string(), "");
+
+        let q = q.filter_by("name:{}", "foo bar");
+        assert_eq!(q.filter_string(), "name:foo%20bar");
+
+        let q = q.filter_by("city:{}*", "Los ");
+        assert_eq!(q.filter_string(), "name:foo%20bar,city:Los%20*");
+
+        let q = q.filter_by("count:<{}", 42);
+        assert_eq!(q.filter_string(), "name:foo%20bar,city:Los%20*,count:<42");
+
+        let q = q.filter_by("login:{}", true);
+        assert_eq!(
+            q.filter_string(),
+            "name:foo%20bar,city:Los%20*,count:<42,login:true"
+        );
     }
 }
